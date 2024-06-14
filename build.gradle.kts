9d5e485b11e84ca8c9d2c64ec3d63ac1db8659cf
Borychev Andrey <borychev.au@gmail.com>
2024-06-14 22:32:17 +0300

    test docker 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     // Annotation processors
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    // Annotation for docker deploy
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.1")
 
     // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
